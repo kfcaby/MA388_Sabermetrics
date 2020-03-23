1649058e487bc790e4e8c8321bd76ae5cf44b6eb
kfcaby <kfcaby@gmail.com>
2020-03-23 12:31:15 -0400

    added lesson 23 material from class

--- ARod_Lesson22.R
+++ ARod_Lesson22.R
@@ -19,17 +19,57 @@ get_stats(arod.id) -> ARod
 
 # plot data
 
-# fit model and extract coefficients
+ARod %>% 
+  ggplot(aes(x = Age, y = OPS)) +
+  geom_point() + labs("ARod") +
+  geom_smooth(method = "lm",
+              formula = y ~ x + I(x^2),
+              se = FALSE)
 
-# What is A (Poll)?
+fit_model(ARod)
 
 # Compare to other players
 
+player.ids %>% 
+  map_df(get_stats) %>% 
+  left_join(Master %>% select(nameLast, playerID)) -> player.stats
+
+player.stats %>% 
+  ggplot(aes(x = Age, y = OPS)) +
+  geom_point() +
+  geom_smooth(method = "lm",
+              formula = y ~ x + I(x^2),
+              se = FALSE) +
+  facet_wrap( ~ nameLast)
+
 # Which player has the largest C in magnitude (poll)
+
+player.stats %>% 
+  split(pull(.,nameLast)) %>% 
+  map(fit_model)
+
+#Arod  C = -0.003
 
 # Breaking these models is pretty easy (see Ortiz)
 
+# Usually, we would use these to predict future performance
 
+get_stats("ortizda01") -> Ortiz
 
-           
+#fit the quadratic model to Big Papi at 29 years old
+
+Ortiz %>% filter(Age < 30) -> Ortiz.young
+
+fit_model(Ortiz.young)
+
+#problem--- Ortiz has a positive C!!!!!
+
+Ortiz %>% 
+  mutate(OPS_predict = 1.2323 + 0.1532*(Age-30) + 0.01059*(Age-30)^2) -> Ortiz
+
+Ortiz %>% 
+  ggplot(aes(x = Age, y = OPS_predict)) +
+  geom_line() +
+  geom_point(aes(x = Age, y = OPS))
+
 
